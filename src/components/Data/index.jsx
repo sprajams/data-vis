a5baab9584feefdad61bb61bd9e8386996671fb0
Suphasuta Prajamsri <suphprajam95@gmail.com>
2022-06-15 10:51:29 -0700

    create object to keep track # of duplicate data

--- src/components/Data/index.jsx
+++ src/components/Data/index.jsx
@@ -1,7 +1,8 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function Data() {
   const [data, setData] = useState([]);
+  const [dataObj, setDataObj] = useState({});
 
   const fetchData = () => {
     fetch(
@@ -9,12 +10,33 @@ function Data() {
     )
       .then((res) => res.text())
       .then((data) =>
-        // data received as plain text, convert into an array of intergers
+        // data received as plain text, convert into an array of integers, ignore last element of array since it's NaN
         setData(() => {
-          return data.split("\n").map((x) => parseInt(x));
+          return data
+            .split("\n")
+            .map((x) => {
+              return parseInt(x);
+            })
+            .slice(0, -1);
         })
       );
   };
+  useEffect(() => {
+    // sort data from lower to highest
+    data.sort((a, b) => a - b);
+    // create an object to keep track of how many duplicates are in the data array
+    const obj = {};
+    data.forEach((x) => {
+      if (obj[x]) {
+        obj[x] += 1;
+      } else {
+        obj[x] = 1;
+      }
+    });
+    setDataObj(obj);
+  }, [data]);
+
+  console.log(dataObj);
 
   return (
     <div>
